f45ab6c302ec0cb2176e1da200079ac833fbb134
William Lachance <wrlach@gmail.com>
2009-01-04 18:13:03 -0400

    Make testgraph program take a graph as an argument.
    We should eventually make it much more flexible than this.

--- examples/testgraph.cc
+++ examples/testgraph.cc
@@ -2,8 +2,14 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc < 2)
+    {
+        printf("Usage: %s <graph file>\n", argv[0]);
+        exit(1);
+    }
+
     TripGraph g;
-    g.load("mygraph.routez");
+    g.load(argv[1]);
 
     TripPath p = g.find_path(46167, "weekday", false, 44.657304, -63.591096, 
                 44.7321379, -63.657304);
