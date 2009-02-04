77fa21143bbe03a5ee6c0de7b0fb4a6a1ee80c62
William Lachance <wrlach@gmail.com>
2009-02-04 16:51:08 -0400

    creategraph.py now generates an idmap (yaml) format of gtfs->libroutez ids
    Essential for those who are creating a third party application based on
    libroutez.

--- utils/creategraph.py
+++ utils/creategraph.py
@@ -5,11 +5,40 @@ import libroutez.osm as osm
 import sys
 from libroutez.tripgraph import *
 
-def load_gtfs(tripgraph, sched):
+class IdMap:
+    '''class which maps from gtfs ids -> libroutez ids'''
+    def __init__(self):
+        self.stopmap = {}
+        self.routemap = {}
+        self.tripmap = {}
+
+    def save(self, fname):
+        f = open(fname, 'w')
+        
+        print >> f, "Stops: {"
+        for gtfs_stop_id in sorted(self.stopmap.keys()):
+            print >>f, "    '%s': %s," % (gtfs_stop_id, self.stopmap[gtfs_stop_id])
+        print >> f, "}"
+
+        print >> f, "Routes: {"
+        for gtfs_route_id in sorted(self.routemap.keys()):
+            print >>f, "    '%s': %s," % (gtfs_route_id, 
+                                      self.routemap[gtfs_route_id])
+        print >> f, "}"
+
+        print >> f, "Trips: {"
+        for gtfs_trip_id in sorted(self.tripmap.keys()):
+            print >> f, "    '%s': %s," % (gtfs_trip_id, self.tripmap[gtfs_trip_id])
+        print >> f, "}"
+
+        f.close()
+
+def load_gtfs(tripgraph, sched, idmap):
     stops = sched.GetStopList()
     for stop in stops:
-      tripgraph.add_tripstop(int(stop.stop_id), "gtfs", stop.stop_lat, 
-                        stop.stop_lon)
+        idmap.stopmap[stop.stop_id] = len(idmap.stopmap)
+        tripgraph.add_tripstop(idmap.stopmap[stop.stop_id], "gtfs", 
+                               stop.stop_lat, stop.stop_lon)
       
     trips = sched.GetTripList()
     for trip in trips:
@@ -24,48 +53,55 @@ def load_gtfs(tripgraph, sched):
             print "WARNING: Negative edge in gtfs. This probably means you "
             "need a more recent version of the google transit feed " 
             "package (see README)"
-          tripgraph.add_triphop(prevsecs, secs, int(prevstop.stop_id),
-                                int(stop.stop_id), int(trip.route_id), 
-                                int(trip.trip_id), str(trip.service_id))        
+          if not idmap.tripmap.get(trip.trip_id):
+              idmap.tripmap[trip.trip_id] = len(idmap.tripmap)
+          if not idmap.routemap.get(trip.route_id):
+              idmap.routemap[trip.route_id] = len(idmap.routemap)
+          tripgraph.add_triphop(prevsecs, secs, idmap.stopmap[prevstop.stop_id],
+                                idmap.stopmap[stop.stop_id], 
+                                idmap.routemap[trip.route_id], 
+                                idmap.tripmap[trip.trip_id], 
+                                str(trip.service_id))
         prevstop = stop
         prevsecs = secs
 
-def load_osm(tripgraph, map, startid):
+def load_osm(tripgraph, map, idmap):
     # map of osm ids -> libroutez ids. libroutez ids are positive integers, 
     # starting from the last gtfs id. I'm assuming that OSM ids can be pretty 
     # much anything
-    nodemap = {}
-    curid = startid
-
     for node in map.nodes.values():
-        nodemap[node.id] = curid
-        tripgraph.add_tripstop(curid, "osm", node.lat, node.lon)
-        curid += 1
+        idmap.stopmap[node.id] = len(idmap.stopmap)
+        tripgraph.add_tripstop(idmap.stopmap[node.id], "osm", node.lat, 
+                               node.lon)
         
     for way in map.ways.values():
         prev_id = None
         for id in way.nds:
             if prev_id:
-                tripgraph.add_walkhop(nodemap[prev_id], nodemap[id])
-                tripgraph.add_walkhop(nodemap[id], nodemap[prev_id])
+                tripgraph.add_walkhop(idmap.stopmap[prev_id], idmap.stopmap[id])
+                tripgraph.add_walkhop(idmap.stopmap[id], idmap.stopmap[prev_id])
             prev_id = id
 
 if __name__ == '__main__':
-    if len(sys.argv) < 4:
-        print "Usage: %s: <gtfs feed> <osm file> <graph>" % sys.argv[0]
+    if len(sys.argv) < 5:
+        print "Usage: %s: <gtfs feed> <osm file> <graph> <gtfs mapping>" \
+            % sys.argv[0]
         exit(1)
 
     g = TripGraph()
-    schedule = transitfeed.Schedule(
-        problem_reporter=transitfeed.ProblemReporter())
-    print "Loading schedule."
-    schedule.Load(sys.argv[1])
+    idmap = IdMap()
     print "Loading OSM."
     map = osm.OSM(sys.argv[2])
+    print "Loading schedule."
+    schedule = transitfeed.Schedule(
+        problem_reporter=transitfeed.ProblemReporter())
+    schedule.Load(sys.argv[1])
     print "Inserting gtfs into graph"
-    load_gtfs(g, schedule)
+    load_gtfs(g, schedule, idmap)
     print "Inserting osm into graph"
-    load_osm(g, map, len(schedule.GetStopList()))
+    load_osm(g, map, idmap)
+    print "Saving idmap"
+    idmap.save(sys.argv[4])
     print "Linking osm with gtfs"
     g.link_osm_gtfs()
     print "Saving graph"
