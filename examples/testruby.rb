ddcbf38c6cff925ceb0191f87ce702f59d8ac847
William Lachance <wrlach@gmail.com>
2009-09-11 18:14:18 -0400

    Update ruby example for new api

--- examples/testruby.rb
+++ examples/testruby.rb
@@ -16,14 +16,14 @@ g.add_tripstop(0, Routez::TripStop::OSM, 0.0, 0.0)
 g.add_tripstop(1, Routez::TripStop::OSM, 1.0, 0.0)
 g.add_walkhop(0, 1)
 
-path = g.find_path(0, "caturday", false, 0.0, 0.0, 1.0, 0.0)
+path = g.find_path(0, false, 0.0, 0.0, 1.0, 0.0)
 
 path.get_actions().each do |action|
   puts "src: #{action.src_id} dest: #{action.dest_id} st: #{action.start_time} et: #{action.end_time} rid: #{action.route_id}" 
 end
 
-g.add_triphop(500, 1000, 0, 1, 1, 1, "caturday")
-path2 = g.find_path(0, "caturday", false, 0.0, 0.0, 1.0, 0.0)
+g.add_triphop(500, 1000, 0, 1, 1, 1, 0)
+path2 = g.find_path(0, false, 0.0, 0.0, 1.0, 0.0)
 
 path2.get_actions().each do |action|
   puts "src: #{action.src_id} dest: #{action.dest_id} st: #{action.start_time} et: #{action.end_time} rid: #{action.route_id}" 
