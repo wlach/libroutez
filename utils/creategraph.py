59da2436543693f5f7dbd4fdcb4f5ffc198f10dd
William Lachance <wrlach@gmail.com>
2010-04-09 13:41:47 -0400

    Many improvements to the OSM<->GTFS linking step
    Linking is now done with the creategraph utility, and uses Python Rtree to
    find nearest nodes, for an order of magnitude speed up. Also, we now link
    inside way boundaries, splitting the way if necessary. This fixes a number
    of weird situations that could crop up where we would link long distances
    to a way.

--- utils/creategraph.py
+++ utils/creategraph.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
 import transitfeed
+from rtree import Rtree
 import libroutez.osm as osm
+import math
 import time
 import sys
 from libroutez.tripgraph import *
@@ -41,7 +43,7 @@ class IdMap:
 
         f.close()
 
-def load_gtfs(tripgraph, sched, idmap):
+def process_gtfs(tripgraph, sched, idmap):
     print "Setting timezone to %s" % sched.GetDefaultAgency().agency_timezone
     tripgraph.set_timezone(str(sched.GetDefaultAgency().agency_timezone))
 
@@ -82,7 +84,8 @@ def load_gtfs(tripgraph, sched, idmap):
           if prevstop.stop_id != stop.stop_id:
               # only add triphop if we're not going to ourselves. there are
               # some feeds (cough, cough, Halifax) which actually do this
-              tripgraph.add_triphop(prevsecs, secs, idmap.stopmap[prevstop.stop_id],
+              tripgraph.add_triphop(prevsecs, secs, 
+                                    idmap.stopmap[prevstop.stop_id],
                                     idmap.stopmap[stop.stop_id], 
                                     idmap.routemap[trip.route_id], 
                                     idmap.tripmap[trip.trip_id], 
@@ -122,23 +125,178 @@ def load_gtfs(tripgraph, sched, idmap):
             "bound. This implies that it's not actually being used for anything."
 
 
-def load_osm(tripgraph, map, idmap):
+def process_osm(tripgraph, osm_map, sched, idmap):
+
+    # first, we create a new map with the appropriate nodes to be able
+    # to link with the transit graph
+    print "Rewriting map to link with GTFS..."
+
+    link_map = {}
+
+    class WayIndex:
+        def __init__(self, ways):
+            self.max_way_id = 0
+            self.way_idx_mapping = {}
+            self.idx = Rtree()
+            for way in ways:
+                self.add(way)
+
+        def add(self, way):
+            self.idx.add(id=self.max_way_id, coordinates=way.get_bbox(), 
+                         obj=way.id)
+            self.way_idx_mapping[way.id] = self.max_way_id
+            self.max_way_id += 1
+
+        def delete(self, way):
+            self.idx.delete(id=self.way_idx_mapping[way.id],
+                            coordinates=way.get_bbox())
+            way_ids = map(lambda way: way.object, 
+                          wayidx.idx.intersection(way.get_bbox(), 
+                                                  objects=True))
+
+    wayidx = WayIndex(osm_map.ways.values())
+    
+    MIN_DISTANCE=0.00075
+    
+    for stop in sched.GetStopList():
+        way_ids = map(lambda way: way.object, 
+                      wayidx.idx.intersection((stop.stop_lon-MIN_DISTANCE, 
+                                        stop.stop_lat-MIN_DISTANCE, 
+                                        stop.stop_lon+MIN_DISTANCE, 
+                                        stop.stop_lat+MIN_DISTANCE), 
+                                       objects=True))
+    
+        # fall back to doing an exhaustive search if we can't find a way in 
+        # range
+        if len(way_ids) == 0:        
+            way_ids = osm_map.ways.keys()
+    
+        class ClosestWay:
+            def __init__(self):
+                self.way = None # way
+                self.subway_nds = None # subway indices, scalar magnitude along subway
+                self.dist = None
+                self.intersection_pt = None
+                self.intersection_scalar = None
+                
+        closest_way = ClosestWay()
+    
+        def latlng_dist(src_lat, src_lng, dest_lat, dest_lng):
+            theta = src_lng - dest_lng
+            src_lat_radians = math.radians(src_lat)
+            dest_lat_radians = math.radians(dest_lat)
+            dist = math.sin(src_lat_radians) * math.sin(dest_lat_radians) + \
+                math.cos(src_lat_radians) * math.cos(dest_lat_radians) * \
+                math.cos(math.radians(theta))
+            dist = math.degrees(math.acos(dist)) * (60.0 * 1.1515 * 1.609344 * 1000.0)
+            return dist
+    
+        for way_id in way_ids:
+            way = osm_map.ways[way_id]
+            prev_id = None
+            for id in way.nds:
+                if prev_id:
+                    prev_node = osm_map.nodes[prev_id]
+                    node = osm_map.nodes[id]
+    
+                    # slope of the sub-way
+                    slope1_lon = node.lon - prev_node.lon
+                    slope1_lat = node.lat - prev_node.lat
+                    
+                    # slope of the vector from the stop to the sub way
+                    slope2_lon = slope1_lat * (-1)
+                    slope2_lat = slope1_lon 
+    
+                    # the scalar value of the intersection on the way: 
+                    # - < 0 = before the first pt,
+                    # - > 0 = after the last pt 
+                    # - between = between the two points
+                    # FIXME: use an epsilon, not zero
+                    intersection_scalar = (((prev_node.lon - stop.stop_lon) * slope2_lat + \
+                                                (stop.stop_lat - prev_node.lat) * slope2_lon) / \
+                                               (slope1_lat*slope2_lon - slope1_lon*slope2_lat))
+                    way_dist = None
+                    intersection_pt = None
+                    EPSILON=0.0005
+                    if intersection_scalar <= EPSILON:
+                        way_dist = latlng_dist(prev_node.lat, prev_node.lon, stop.stop_lat, stop.stop_lon)
+                    elif intersection_scalar >= (1.0 - EPSILON):
+                        way_dist = latlng_dist(node.lat, node.lon, stop.stop_lat, stop.stop_lon)
+                    else:
+                        intersection_pt = (prev_node.lat+(intersection_scalar*slope1_lat), 
+                                           prev_node.lon+(intersection_scalar*slope1_lon))
+                        way_dist = latlng_dist(intersection_pt[0], intersection_pt[1], stop.stop_lat, stop.stop_lon)
+                   
+                    if not closest_way.way or way_dist < closest_way.dist:
+                        closest_way.way = way
+                        closest_way.dist = way_dist
+                        closest_way.subway_nds = (prev_id, id)
+                        closest_way.intersection_scalar = intersection_scalar
+                        closest_way.intersection_pt = intersection_pt
+                prev_id = id
+
+        # ok, now that we have a closest way, act accordingly...
+        if closest_way.intersection_scalar > EPSILON and \
+                closest_way.intersection_scalar < 1.0 - EPSILON:
+            # append closest_intersection_pt to nds
+            indice = closest_way.way.nds.index(closest_way.subway_nds[1])
+            new_node_id = "newnode-" + str(len(osm_map.nodes.keys()))
+            osm_map.nodes[new_node_id] = osm.Node(new_node_id, 
+                                                  closest_way.intersection_pt[1], 
+                                                  closest_way.intersection_pt[0])
+
+            # split way in two
+            new_way_id1 = closest_way.way.id + "-1"
+            new_way1 = osm.Way(new_way_id1, osm_map)
+            new_way1.nds = closest_way.way.nds[0:indice]
+            new_way1.nds.append(new_node_id)
+    
+            new_way_id2 = closest_way.way.id + "-2"
+            new_way2 = osm.Way(new_way_id2, osm_map)
+            new_way2.nds = closest_way.way.nds[indice:]
+            new_way2.nds.insert(0, new_node_id)
+    
+            wayidx.delete(closest_way.way)
+            del osm_map.ways[closest_way.way.id]
+    
+            osm_map.ways[new_way_id1] = new_way1
+            wayidx.add(new_way1)
+            osm_map.ways[new_way_id2] = new_way2
+            wayidx.add(new_way2)
+
+            link_map[stop.stop_id] = new_node_id
+    
+        elif closest_way.intersection_scalar <= EPSILON:        
+            link_map[stop.stop_id] = closest_way.subway_nds[0]
+    
+        else: # closest_way.intersection_scale >= EPSILON
+            link_map[stop.stop_id] = closest_way.subway_nds[-1]
+    
     # map of osm ids -> libroutez ids. libroutez ids are positive integers, 
     # starting from the last gtfs id. I'm assuming that OSM ids can be pretty 
     # much anything
+    print "Adding OSM to graph..."
     osm_nodemap = {}
-    for node in map.nodes.values():
+    for node in osm_map.nodes.values():
         osm_nodemap[node.id] = len(osm_nodemap) + len(idmap.stopmap)
         tripgraph.add_tripstop(osm_nodemap[node.id], TripStop.OSM,
                                node.lat, node.lon)
-        
-    for way in map.ways.values():
+    
+    for way in osm_map.ways.values():
         previd = None
         for id in way.nds:
             if previd:
                 tripgraph.add_walkhop(osm_nodemap[previd], osm_nodemap[id])
                 tripgraph.add_walkhop(osm_nodemap[id], osm_nodemap[previd])
+
             previd = id
+
+    print "Adding GTFS->OSM links to graph..."
+    for stop_id in link_map.keys():
+        tripgraph.add_walkhop(idmap.stopmap[stop_id],
+                              osm_nodemap[link_map[stop_id]])
+        tripgraph.add_walkhop(osm_nodemap[link_map[stop_id]],
+                              idmap.stopmap[stop_id])
 
 if __name__ == '__main__':
 
@@ -161,15 +319,13 @@ if __name__ == '__main__':
     g = TripGraph()
     print "Inserting gtfs into graph"
     idmap = IdMap()
-    load_gtfs(g, schedule, idmap)
+    process_gtfs(g, schedule, idmap)
 
     if options.osm:
         print "Loading OSM."
-        map = osm.OSM(options.osm)
-        print "Inserting osm into graph"
-        load_osm(g, map, idmap)
-        print "Linking osm with gtfs"
-        g.link_osm_gtfs()
+        osm_map = osm.OSM(options.osm)
+
+        process_osm(g, osm_map, schedule, idmap)
 
     print "Saving idmap"
     idmap.save(args[2])
